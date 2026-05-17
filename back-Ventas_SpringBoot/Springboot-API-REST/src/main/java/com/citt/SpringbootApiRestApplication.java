939e7aa50391fe2825c80e3a87fde2e6ed1c596d
danibhern <dan.barrerah@duocuc.cl>
2026-05-16 21:25:25 -0400

    Prueba de workflow en backend

--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/SpringbootApiRestApplication.java
@@ -3,6 +3,10 @@ package com.citt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Backend Tienda Perritos - Ventas API
+ * Version 1.0.0
+ */
 @SpringBootApplication
 public class SpringbootApiRestApplication {
 
